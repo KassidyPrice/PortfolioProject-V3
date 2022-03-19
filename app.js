07a93ea035f5b234fc006fd8bf58c0076fc5752b
KassidyPrice <96833138+KassidyPrice@users.noreply.github.com>
2022-03-19 15:38:12 -0600

    3:38

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ function removeActiveClasses() {
     panel.classList.remove('active')
   })
 }
-// const links = document.querySelectorAll('a')
-// const panels = document.querySelectorAll('.panel')
-// links.forEach((a) => {
-//   a.addEventListener('click', () => {
-//     removeActiveClasses()
-//     panel.classList.add('active')
-//   })
-// })
-
-// function removeActiveClasses() {
-//   panels.forEach((panel) => {
-//     panel.classList.remove('active')
-//   })
-// }
 
 let modal = document.getElementById('myModal')
 let span = document.getElementsByClassName('close')[0]
